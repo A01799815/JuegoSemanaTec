f10caf8f73dfffecba66b8f08000fd3d4d44376b
César Antonio Espinosa Madrid <A01799815@tec.mx>
2023-05-12 08:52:52 -0600

    Se agregaron y cambiaron docstrings y comentarios

--- draw.py
+++ draw.py
@@ -21,6 +21,7 @@ def square(start, end):
 
     end_fill()
 
+#Se utiliza la funcion predeterminada circle()
 #Se renombro la funcion como 'circle_' para evitar errores en la ejecucion al usar tambien la funcion predeterminada circle()
 def circle_(start, end):    
     "Dibuja un circulo desde el punto inicial alrededor del punto indicado como punto final"
@@ -35,10 +36,9 @@ def circle_(start, end):
     end_fill()
     
 
-#Se utliza el mismo metodo del cuadrado, sin embargo añadiento las coordenadas en y para poder hacer el rectangulo 
-# y cambiando el rango de lineas a dibujar 
+#Se utliza el mismo metodo del cuadrado, añadiendo las coordenadas del punto final y cambiando el rango de lineas a dibujar.
 def rectangle(start, end):
-    "Draw rectangle from start to end."
+    "Dibuja un rectangulo con diagonal desde el punto inical al punto final."
     up()
     goto(start.x, start.y)
     down()
@@ -50,10 +50,9 @@ def rectangle(start, end):
         left(90)
     end_fill()
     
-#Se utliza el mismo metodo del cuadrado, pero, se cambia el cambia el rango para que dibuje 3 lineas y 
-# el angulo left cambiarlo a 120
+#Se utliza el mismo metodo del cuadrado, cambiando el rango para que dibuje 3 lineas y # el angulo left cambiarlo a 120
 def triangle(start, end):
-    "Draw triangle from start to end."
+    "Dibuja un triangulo equilatero desde y hasta el punto inicial. El valor del lado es la distancia al punto final."
     up()
     goto(start.x, start.y)
     down()
@@ -67,7 +66,7 @@ def triangle(start, end):
 
 
 def tap(x, y):
-    "Store starting point or draw shape."
+    "Guarda las coordenadas del punto incial si no hay ninguno o dibuja una figura."
     start = state['start']
 
     if start is None:
@@ -79,7 +78,7 @@ def tap(x, y):
         state['start'] = None
 
 def store(key, value):
-    "Store value in state at key."
+    "Guarda el valor del estado actual del dibujo"
     state[key] = value
 
 state = {'start': None, 'shape': line}
@@ -92,7 +91,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('purple'), 'P') #### Se añadio un nuevo color
+onkey(lambda: color('purple'), 'P') # Se añadio un nuevo color
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle_), 'c')
